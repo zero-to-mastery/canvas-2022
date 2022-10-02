952227c77e50ab02870ae724619d59fabafd9c09
avatar-10000 <92681049+avatar-10000@users.noreply.github.com>
2022-10-02 16:44:08 +0530

    Update script.js

--- src/art/avatar/script.js
+++ src/art/avatar/script.js
@@ -1,16 +1,47 @@
-const inputs = document.querySelectorAll(".controls input");
+//initial
+  var w = c.width = window.innerWidth,
+  h = c.height = window.innerHeight,
+  ctx = c.getContext('2d'),
+  
+  //parameters
+  total = w,
+  accelleration = .05,
+  
+  //afterinitial calculations
+  size = w/total,
+  occupation = w/total,
+  repaintColor = 'rgba(0, 0, 0, .04)'
+  colors = [],
+  dots = [],
+  dotsVel = [];
 
-            function handleUpdate() {
-                const suffix = this.dataset.sizing || "";
-                document.documentElement.style.setProperty(
-                    `--${this.name}`,
-                    this.value + suffix
-                );
-            }
+//setting the colors' hue
+//and y level for all dots
+var portion = 360/total;
+for(var i = 0; i < total; ++i){
+colors[i] = portion * i;
 
-            inputs.forEach((input) =>
-                input.addEventListener("change", handleUpdate)
-            );
-            inputs.forEach((input) =>
-                input.addEventListener("mousemove", handleUpdate)
-            );
+dots[i] = h;
+dotsVel[i] = 10;
+}
+
+function anim(){
+window.requestAnimationFrame(anim);
+
+ctx.fillStyle = repaintColor;
+ctx.fillRect(0, 0, w, h);
+
+for(var i = 0; i < total; ++i){
+  var currentY = dots[i] - 1;
+  dots[i] += dotsVel[i] += accelleration;
+  
+  ctx.fillStyle = 'hsl('+ colors[i] + ', 80%, 50%)';
+  ctx.fillRect(occupation * i, currentY, size, dotsVel[i] + 1);
+  
+  if(dots[i] > h && Math.random() < .01){
+    dots[i] = dotsVel[i] = 0;
+  }
+}
+}
+
+anim();
