c8599afe9c706cc2150d85cead5c308a130b6bb9
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-10-02 23:56:23 +0530

    Added new animation

--- src/art/wendemsky/scripts.js
+++ src/art/wendemsky/scripts.js
@@ -54,5 +54,48 @@ c.lineTo(200,600);
 c.lineTo(350,350);
 c.lineTo(500,600);
 c.lineTo(600,100)
-c.strokeStyle = "rgba(0, 0, 0, 1)";
+c.strokeStyle = "teal";
 c.stroke();
+
+
+const circle = {
+    x: 200,
+    y: 200,
+    size: 40,
+    dx: 5,
+    dy: 4
+}
+
+function drawCircle() {
+   
+    c.beginPath();
+    // c.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+    c.rect(circle.x, circle.y, circle.size, circle.size );
+    c.fillStyle = 'pink';
+    c.fill();
+}
+
+function update(){
+    // c.clearRect(0, 0, canvas.width, canvas.height);
+    
+ 
+    drawCircle();
+    c.clearRect(circle.x + circle.size, circle.y + circle.size, 40, 40);
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+    
+    //Collision Detection
+    if(circle.x + circle.size > canvas.width || circle.x - circle.size < 0){
+        circle.dx *= -1;
+    }
+    if(circle.y + circle.size > canvas.height || circle.y - circle.size < 0){
+        circle.dy *= -1;
+    }
+   
+
+    
+    requestAnimationFrame(update);
+    
+}
+
+update();
