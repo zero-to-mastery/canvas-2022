d63fac956daad6f13fd87669aef818f09df95f5f
anayatkhan1 <anayat0khan@gmail.com>
2022-10-12 23:25:03 +0530

    Remove unused code and unnecessary

--- src/art/AnayatCanvas/art.js
+++ src/art/AnayatCanvas/art.js
@@ -1,9 +1,7 @@
-
 var canvas = document.querySelector('canvas'); 
 canvas.width = window.innerWidth;
 canvas.height  = window.innerHeight;
-
-var context = canvas.getContext('2d'); // getCOntext method is used for making line, draw & so on.
+var context = canvas.getContext('2d'); 
 
 var mouse = {
     
@@ -12,7 +10,7 @@ var mouse = {
 }
 
 var maxRadius = 40;
-// var minRadius = 2;
+
 
 var colorArray = [
     "#FDFF00",
@@ -22,15 +20,12 @@ var colorArray = [
     "#87A2FB"
 ]
 
-// add EventListener to detect mouse behave
-
 window.addEventListener('mousemove', function(event){
 
     mouse.x = event.x;
     mouse.y = event.y;
 });
 
-// web browser dynamically fixing
 
 window.addEventListener('resize', function(){
 
@@ -49,7 +44,7 @@ function Circle(x,y, dx, dy, radius)
     this.dy = dy;
     this.radius = radius;
     this.minRadius = radius;
-    this.color = colorArray[Math.floor(Math.random() * colorArray.length)]; // math.Floor is used for lowest Whole number
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)]; 
 
     this.draw = function(){
 
@@ -118,14 +113,10 @@ function init()
 
 }
 
-// console.log(circleArray);
-
-
-// This function is used for making Animation
 function Animate()
 
 {
-    requestAnimationFrame(Animate); // requestAnimationFrame is function which provides looping callback over and over again
+    requestAnimationFrame(Animate); 
     context.clearRect(0,0,innerWidth, innerHeight);
 
     for(var i  = 0  ; i<circleArray.length ; i++)
@@ -145,36 +136,3 @@ function Animate()
 
 init();
 Animate();
-
-// Below Code is for practicing for making animation, canvas and So on.
-
-// context.fillStyle = 'rgba(255,0,0,0.5)';
-// context.fillRect(100,100,100,100);
-// context.fillStyle = 'rgba(0,255,0,0.5)';
-// context.fillRect(400,100,100,100);
-// context.fillStyle = 'rgba(0,0,255,0.5)';
-// context.fillRect(300,300,100,100);
-
-// console.log(canvas);
-
-// // lIne
-
-// context.beginPath();
-// context.moveTo(50,300);
-// context.lineTo(300,100);
-// context.lineTo(400,300);
-// context.strokeStyle = "green";
-// context.stroke();
-
-// Arc / Cricle
-
-// for(var i = 0  ; i<100 ; i++)
-// {
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
-//     context.beginPath();
-//     context.arc(x,y,30,0, Math.PI * 2, false);
-//     context.strokeStyle = "red";
-//     context.stroke();
-
-// }
