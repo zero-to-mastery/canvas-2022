e3fb493e79e8692a8b233544f7173e40a6a2f561
vignesh_gopal <vigneshg@bni.com>
2022-10-13 17:13:10 +0530

    Added canavas owl smiley eyebrows

---
+++ src/art/vigneshg1616/script.js
@@ -0,0 +1,15 @@
+
+
+var c = document.getElementById("canvas-eyebrow-1");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 10);
+ctx.lineTo(350, 80);
+ctx.lineWidth = 10;
+ctx.stroke();
+
+var c = document.getElementById("canvas-eyebrow-2");
+var ctx = c.getContext("2d");
+ctx.moveTo(50, 50);
+ctx.lineTo(950, 0);
+ctx.lineWidth = 10;
+ctx.stroke();
