a2bf3e614bf4978f7a5debcdb185b69e7b216576
Ben Sivyer <bensivyer@Bens-MacBook-Pro.local>
2022-10-14 16:20:45 +0100

    friday evening update

---
+++ src/art/BGS89/script.js
@@ -0,0 +1,21 @@
+function draw() {
+    const canvas = document.getElementById('test');
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+
+        // ctx.fillStyle = "rgb(200, 0, 0)";
+        // ctx.fillRect(10, 10, 50, 50);
+
+        // ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        // ctx.fillRect(30, 30, 50, 50);
+
+        ctx.fillStyle = 'rgb(255, 0, 0)';
+        ctx.fillRect(123, 0, 4, 150);
+
+
+
+    }
+ 
+}
+
+draw();
