c7f174184bbf8c6a5e718ee8c6fcf9483b7ca130
nikimacm <nickymacm@gmail.com>
2022-10-19 13:21:25 +0000

    practice tests with canvas

--- src/art/nicola/script.js
+++ src/art/nicola/script.js
@@ -1,1 +1,10 @@
-console.log('is it working');
+function draw() {
+    const canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d');
+  
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    }
+  }
