7f348fee6072d411d0bbeebc4dafe9411db7ecc5
Your Name <you@example.com>
2022-10-19 21:18:57 +0530

    Changing the file name of screenshot

--- src/art/Anurag/script.js
+++ src/art/Anurag/script.js
@@ -1,16 +1,12 @@
-    //Grab the canvas element on the client 
+//Grab the canvas element on the client 
     // and get the context to add stuff to the canvas
     var canvas = document.getElementById("myCanvas");
-    var CanvasContext = canvas.getContext("2d");
+    var canvasContext = canvas.getContext("2d");
 
-    //Since we are going to use the line 
-    // to draw the entire logo, set up
-    // some properties to get the right width
-    // color, the cap and join style 
-    CanvasContext.lineWidth = 50;
-    CanvasContext.strokeStyle = "#F6FD04";
-    CanvasContext.lineCap = "square";
-    CanvasContext.lineJoin = "round";  
+    canvasContext.lineWidth = 50;
+    canvasContext.strokeStyle = "#F6FD04";
+    canvasContext.lineCap = "square";
+    canvasContext.lineJoin = "round";  
 
     //Through out our drawing process we will just
     // call the moveTo method in case we want draw 
@@ -18,37 +14,37 @@
     // lineTo so that the drawing operation takes place
     // when we call the stroke method
 
-    //Draw the "i" dot
-    CanvasContext.moveTo(50, 50);
-    CanvasContext.lineTo(50, 52);
+    //Draw the "i" dot at the top left
+    canvasContext.moveTo(50, 50);
+    canvasContext.lineTo(50, 52);
 
     //Draw line on the left
-    CanvasContext.moveTo(50, 130);
-    CanvasContext.lineTo(50, 200);
+    canvasContext.moveTo(50, 130);
+    canvasContext.lineTo(50, 200);
 
     //Draw the base line
-    CanvasContext.lineTo(220, 200);
+    canvasContext.lineTo(220, 200);
 
     //Change the line join to get a square edge
     // at the bottom right and then switch back to round
-    CanvasContext.lineJoin = "round";
-    CanvasContext.stroke();
-    CanvasContext.lineJoin = "round";
+    canvasContext.lineJoin = "round";
+    canvasContext.stroke();
+    canvasContext.lineJoin = "round";
 
     //Draw line on the right
-    CanvasContext.lineTo(220, 50);
+    canvasContext.lineTo(220, 50);
 
     //Draw the top line
-    CanvasContext.lineTo(120, 50);
+    canvasContext.lineTo(120, 50);
 
     //Change the line join to get a square edge
     // at the top left
-    CanvasContext.lineJoin = "round";
-    CanvasContext.stroke();
+    canvasContext.lineJoin = "round";
+    canvasContext.stroke();
 
     //Draw the "G" edge
-    CanvasContext.lineTo(120, 150);
+    canvasContext.lineTo(120, 150);
 
     //Finally, call the stroke method so that 
     // all the remaining drawing can be completed
-    CanvasContext.stroke();
+    canvasContext.stroke();
