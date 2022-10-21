b3a324523ced0d7255a514ae5117b68056537a45
Amit Das <91092039+das-amit@users.noreply.github.com>
2022-10-22 00:26:19 +0530

    Added the Flag of Gabon

---
+++ src/art/Amit/script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('amitCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#00833E';
+ctx.fillRect(0, 0, 1000, 103);
+ctx.fillStyle = '#FED116';
+ctx.fillRect(0, 103, 1000, 104);
+ctx.fillStyle = '#0032A1';
+ctx.fillRect(0, 207, 1000, 105);
