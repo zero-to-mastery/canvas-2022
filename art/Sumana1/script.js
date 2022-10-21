e4651db803d2be774bae3ce14fbba7297f6b0eb2
auxfuse <48055438+auxfuse@users.noreply.github.com>
2022-10-21 18:31:04 +0000

    Deploying to gh-pages from  @ fb9179b3fac34b53767c3be570df2d39c615843c 🚀

---
+++ art/Sumana1/script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('sumanaCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(0, 0, 175, 310);
+ctx.fillStyle = 'yellow';
+ctx.fillRect(175, 0, 500, 155);
+ctx.fillStyle = '#F00';
+ctx.fillRect(175, 155, 500, 155);
