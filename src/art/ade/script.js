8f498aed710f2e924171f4428dbdd9c4cc81e4a8
ahmadadejumo <adejumoahmad@gmail.com>
2022-10-25 09:07:00 +0100

    change colors

--- src/art/ade/script.js
+++ src/art/ade/script.js
@@ -1,11 +1,11 @@
 let canvas = document.getElementById("germanFlag");
 let canva = canvas.getContext("2d");
 
-canva.fillStyle = "#FF0000";
+canva.fillStyle = "#000000";
 canva.fillRect(0, 0, canvas.width, canvas.height / 3);
 
-canva.fillStyle = "#FFFFFF";
+canva.fillStyle = "#FF0000";
 canva.fillRect(0, canvas.height / 3, canvas.width, canvas.height / 3);
 
-canva.fillStyle = "#000000";
+canva.fillStyle = "#FFFF00";
 canva.fillRect(0, (canvas.height / 3) * 2, canvas.width, canvas.height / 3);
