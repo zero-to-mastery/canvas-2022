69add37a84d9144a0a376fc6eeaef29857123ccc
Rubi <grubi1328@gmail.com>
2022-10-28 17:06:56 +0530

    Added Flag of Madagascar

---
+++ src/art/RubiGupta2/script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('rubiCanvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, 150, 310);
+ctx.fillStyle = 'red';
+ctx.fillRect(150, 0, 500, 155);
+ctx.fillStyle = 'green';
+ctx.fillRect(150, 155, 500, 155);
