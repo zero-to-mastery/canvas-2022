ec3da4a1e1bd9a283cc4904242d294f4c87ce872
auxfuse <48055438+auxfuse@users.noreply.github.com>
2022-10-28 12:54:05 +0000

    Deploying to gh-pages from  @ 5ec9fd0883e47b53e0d61111b319af861cfd1ba4 🚀

---
+++ art/RubiGupta3/script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('rubiCanvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+ctx.fillRect(0, 0, 175, 310);
+ctx.fillStyle = 'yellow';
+ctx.fillRect(175, 0, 500, 155);
+ctx.fillStyle = '#F00';
+ctx.fillRect(175, 155, 500, 155);
