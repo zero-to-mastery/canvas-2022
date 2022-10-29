050c2c17d403fdfc59949458f672189822194df9
Yasin Silavi <ys.silavi@gmail.com>
2022-10-29 12:05:26 +0330

    add smile art

---
+++ src/art/smile/script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+ctx.moveTo(110, 75);
+ctx.arc(75, 75, 35, 0, Math.PI, false);
+ctx.moveTo(65, 65);
+ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
+ctx.moveTo(95, 65);
+ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
+ctx.stroke();
