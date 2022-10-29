4ebbf28c7428d081b3ba9cc8a3438a38399fc5f8
sophiabrandt <16630701+sophiabrandt@users.noreply.github.com>
2022-10-29 13:53:06 +0000

    Deploying to gh-pages from  @ 6973ab67193f81b6d8079c85ae491853ba3c8896 🚀

---
+++ art/anshuman_one/script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("one");
+const ctx = canvas.getContext("2d");
+ctx.font = "350px Arial";
+ctx.fillStyle = "#d90429";
+ctx.fillText("NANDAN", 0, 450);
+ctx.fillStyle = "#fbff12";
+ctx.fillText("NANDAN", 10, 450);
+ctx.fillStyle = "#00a6a6";
+ctx.fillText("NANDAN",20 , 450);
