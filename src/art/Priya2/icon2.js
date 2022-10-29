9962181ed18a8d76d3a76a3536913f8edfb78e86
PriyaDeveloper630 <priyamalik630@gmail.com>
2022-10-29 20:06:31 +0530

    added my art2

--- src/art/Priya2/icon2.js
+++ src/art/Priya2/icon2.js
@@ -1,21 +1,40 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(100, 75, 20, 0, 2 * Math.PI);
-ctx.fillStyle = "yellow";
-ctx.fill();
-ctx.stroke();
-var canvas = document.getElementById('myCanvas');
-  if (canvas.getContext){
-    var ctx1 = canvas.getContext('2d');
-    let height = 200 * Math.cos(Math.PI / 6);
-    ctx1.beginPath();
-    ctx1.moveTo(100,300);
-    ctx1.lineTo(300,300);
-    ctx1.lineTo(200,300-height);
-    ctx1.closePath();
-    
-    ctx1.fillStyle = "red";
-    ctx1.fill();
-    ctx1.stroke();
-  }
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+let cw = canvas.width = 300;// the width of the canvas
+let ch = canvas.height = 300;// the height of the canvas
+
+let c={// the circle: coords of the center and the radius
+  x:75,y:100,r:55
+}
+
+let angle = (2*Math.PI)/3;// the angle between vertices
+
+points = [];// the vertices array
+   
+
+for(let i = 0; i < 3; i++){
+  let o = {}
+  o.x = c.x + c.r*Math.cos(i*angle);
+  o.y = c.y + c.r*Math.sin(i*angle);
+  points.push(o); 
+}
+
+// draw the circle
+context.beginPath();
+context.arc(c.x,c.y,c.r,0,2 * Math.PI);
+context.fillStyle = "blue";
+context.fill();
+context.stroke();
+
+
+// draw the triangle
+context.beginPath();
+context.moveTo(points[0].x,points[0].y);
+for(let i = 1; i < points.length; i++){
+  context.lineTo(points[i].x,points[i].y);
+}
+context.closePath();
+context.fillStyle = "green";
+context.fill();
+context.stroke();
