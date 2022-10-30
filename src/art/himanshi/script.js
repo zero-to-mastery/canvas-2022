59485e26d6794f4f8b05098e5bd59f27af56db75
himanshi4902 <sharmahimanshi0094@gmail.com>
2022-10-31 00:44:43 +0530

    my first contribution

--- src/art/himanshi/script.js
+++ src/art/himanshi/script.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90
-drawClock();
+drawClock(); 
 
 function drawClock() {
   ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
