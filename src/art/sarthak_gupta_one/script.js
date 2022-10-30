2567333fcc08102baf1d8c110e6b22c844b5519f
Sarthak Gupta <sgupta782003@gmail.com>
2022-10-30 23:15:22 +0530

    add-new-canvas

---
+++ src/art/sarthak_gupta_one/script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("SarthakCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.font = "380px Arial";
+ctx.fillStyle = "#d90429";
+ctx.fillText("SARTHAK", 150, 450);
+
+ctx.fillStyle = "#fbff12";
+ctx.fillText("SARTHAK", 170, 450);
+
+ctx.fillStyle = "#00a6a6";
+ctx.fillText("SARTHAK", 190, 450);
