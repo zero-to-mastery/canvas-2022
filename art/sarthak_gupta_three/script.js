94cda350361443009e9f9508365e6d20db17d91e
MattCSmith <matt@matt-smith.dev>
2022-10-31 22:41:43 +0000

    Deploying to gh-pages from  @ d94d6b3da735c7994b349da7f4ef27a77bf30db3 🚀

---
+++ art/sarthak_gupta_three/script.js
@@ -0,0 +1,14 @@
+var canvasElement = document.getElementById("sarthakYoutubeCanvas");
+var ctx = canvasElement.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(130, 100);
+ctx.lineTo(130, 50);
+ctx.lineTo(170, 75);
+ctx.closePath();
+
+ctx.lineWidth = 10;
+ctx.strokeStyle = '#ffffff';
+ctx.stroke();
+ctx.fillStyle = "#ffffff";
+ctx.fill();
