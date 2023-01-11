7db5da79ea4eacfa192b68a250a630cbfa27c1f0
bogdanbanuu <120195028+bogdanbanuu@users.noreply.github.com>
2023-01-11 18:58:06 +0200

    added my canvas

---
+++ src/art/Banu Bogdan/script.js
@@ -0,0 +1,12 @@
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+// Create gradient
+var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);
