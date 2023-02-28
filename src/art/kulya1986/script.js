13c7a0c6d3f891df4bf2a4fa722c5697a7416c9c
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2023-02-28 18:44:46 +0200

    Completed colourful filling the hexagons

--- src/art/kulya1986/script.js
+++ src/art/kulya1986/script.js
@@ -5,9 +5,25 @@ function drawCanvas(){
     let n = 30, i = 26, sides = 6 ; 
     let xInit = 300, yInit = 300; //center of canvas
     let xCenter = [], yCenter = [];//arrays storing center coordinates of each hexagon
-    
-    //Drawing central hexagon(RED)
-    contx.fillStyle = "rgb(255,0,0)";
+    let r=0, g=0, b=0, rStep, gStep, bStep; 
+    let color = [];//color components
+
+
+    let value = 42.5, j=0;
+    for( rStep = 0, r = 0; rStep < 6; rStep++) {    
+        for( gStep = 0, g = 0; gStep < 6; gStep++ ) {       
+            for( bStep = 0, b = 0; bStep < 6; bStep++ ) {  
+                color[j] = "" + r + "," + g + "," + b;
+                b += Math.floor(value);
+                j++;
+            }
+            g += Math.floor(value);
+        }
+        r += Math.floor(value);
+    }
+    // Drawing central hexagon
+    j-=12;
+    contx.fillStyle = `rgb(${color[j]})`;
     drawHexagon(xInit, yInit, n , i);
     contx.fill();
 
@@ -20,28 +36,31 @@ function drawCanvas(){
             yCenter[l+steps1] = yCenter[steps1 - l] = yInit + (steps1-k-l)*i;
             if (l===k)
             {
-                let a=0, b=0;
+                let a=0, s=0;
                 for(a=0; a<=k;a++)
                 {
                     xCenter[l+a]=xInit + l*1.73*i;
                     xCenter[l+steps1+a] = xInit - l*1.73*i;
                 }
                 let steps2 = ((a%2)=== 0) ? a/2 : Math.floor(a/2) + 1;
-                for(b=0,koef = k; b<steps2, koef>=0;b++, koef-=2)
+                for(s=0,koef = k; s<steps2, koef>=0;s++, koef-=2)
                 {
-                    yCenter[l+b] = yCenter[l+steps1+a-1-b] = yInit - koef*i;
-                    yCenter[l+a-1-b] = yCenter[l+steps1+b] = yInit + koef*i;
+                    yCenter[l+s] = yCenter[l+steps1+a-1-s] = yInit - koef*i;
+                    yCenter[l+a-1-s] = yCenter[l+steps1+s] = yInit + koef*i;
                 }
                 l+=a;
                 break;
             }   
         }
+        
         for (let m=0; m<k*sides; m++)
         {
-            contx.strokeStyle = "rgb(255,0,0)";
+            j--;
+            contx.fillStyle = `rgb(${color[j]})`;
             drawHexagon(xCenter[m], yCenter[m], n , i);
-            contx.stroke();
+            contx.fill();
         }
+        j-=12;
     }    
 }
 
