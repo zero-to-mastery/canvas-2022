5d69644457ed95132c82dc2ae2a68ea7914b16e0
azimchamran <m.azims1396@gmail.com>
2023-05-18 08:52:48 +0330

    code refactored

--- src/art/AfghanistanFlag/script.js
+++ src/art/AfghanistanFlag/script.js
@@ -1,21 +1,20 @@
-const flagCanvas = document.getElementById("flag");
-const flagCtx  = flagCanvas.getContext("2d");
+// Drawing the flag of Afghanistan using canvas element and javascript
+function drawFlag() {
+  const flagCanvas = document.getElementById("flag");
+  const flagCtx = flagCanvas.getContext("2d");
 
-const BlackColor = "#000000";
-const GreenColor = "#008053";
-const RedColor = "#D5462A";
+  //* green rectangle
 
-//* green rectangle
-flagCtx.fillStyle = GreenColor;
-flagCtx.fillRect(0, 0, 180*3, 300);
+  flagCtx.fillStyle = "#008053";
+  flagCtx.fillRect(0, 0, 180 * 3, 300);
 
+  // center red rectangel
 
-//* center red rectangel
-flagCtx.fillStyle = RedColor;
-flagCtx.fillRect(0, 0, 180*2, 300);
+  flagCtx.fillStyle = "#D5462A";
+  flagCtx.fillRect(180, 0, 180 * 2, 300);
 
+  // black rectangel
 
-//* black rectangel
-flagCtx.fillStyle = BlackColor;
-flagCtx.fillRect(0, 0, 180, 300);
-
+  flagCtx.fillStyle = "#000000";
+  flagCtx.fillRect(360, 0, 180, 300);
+}
